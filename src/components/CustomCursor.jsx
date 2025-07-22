3473ec2cca0f112a18e1194c2d394eed1a9ef53e
priyanshu22cs25 <priyanshu96p@gmail.com>
2025-07-22 21:22:09 +0530

    herosection + adding spline 3d animation to our page

---
+++ src/components/CustomCursor.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useRef, useEffects } from "react"
+
+const CustomCursor = () => {
+    const cursorRef = useRef(null);
+    const cursorBorderRef = useRef(null);
+  return (
+    <>
+        {/* main cursor dot */}
+        <div>
+            
+        </div>
+
+
+    </>
+  )
+}
+
+export default CustomCursor
