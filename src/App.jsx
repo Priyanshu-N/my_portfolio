28a200cbdddde4479037db6778b78325ef914bfb
priyanshu22cs25 <priyanshu96p@gmail.com>
2025-07-31 19:57:45 +0530

    image upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
 import CustomCursor from "./components/CustomCursor"
 import AboutSection from "./components/AboutSection"
+import ProjectsSection from "./components/ProjectsSection"
 export default function App() {
   return (
     <>
@@ -10,6 +11,7 @@ export default function App() {
       <HeroSection />
       <CustomCursor/>
       <AboutSection/>
+      <ProjectsSection/>
     
     </>
   )
